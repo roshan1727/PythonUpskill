212701d8f60d7dd2e3924c0571268b6e435ddc99
Rakesh Roshan P <57836306+roshan1727@users.noreply.github.com>
2021-06-04 22:50:57 +0530

    Create LamdaFunction.py

---
+++ LamdaFunction.py
@@ -0,0 +1,16 @@
+# print a sum of nummber
+
+add=lambda a,b: a+b
+add(2,4)
+
+# prints value 6
+
+# to print a multiple of number
+a=list(map(int,input().split()))
+a=list(map(lambda a: a*2 ,a))
+print(a)
+
+# innput:
+# a=[2,3,4]
+# output:
+# a=[4,6,8]
